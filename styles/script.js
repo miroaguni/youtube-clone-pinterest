074168a44d8e3abfc2a5230528fde80d7ee4225f
miroaguni <miroaguni@gmail.com>
2025-09-27 18:20:20 -0300

    Correção de importação dos styles.scss
    Por causa do @import './heading'; a animação não estava sendo importada.
    estava headings, com um "s" a mais, causando falha na importação

--- styles/script.js
+++ styles/script.js
@@ -1,24 +1,39 @@
-const grids= document.querySelecterAll('.grid')
-const headings =document.querySelectorAll('.heading .wrapper .text')
+const grids = document.querySelecterAll('.grid')
+const headings = document.querySelectorAll('.heading .wrapper .text')
 
 function enterScreen(index) {
     const grid= grids[index]
-    const heading= heading[index]
+    const heading= headings[index]
     const gridColumns= grid.querySelecterAll('.column')
 
     grid.classList.add('active')
 
     gridColumns.forEach(element => {
-        element.classList.remove('animate-before')
+        element.classList.remove('animate-before','animate-after')
     })
+
+    heading.classList.remove('animate-before','animate-after')
 }
-function exitScreen(index, exitDelay) {}
 
-function setupanimationcycle({
-    initialScreenIndex,
-    timePerScreen,
-    exitDelay}){
+function exitScreen(index, exitDelay) {
+    const grid= grids[index]
+    const heading= headings[index]
+    const gridColumns= grid.querySelecterAll('.column')
 
+     gridColumns.forEach(element => {
+        element.classList.add('animate-after')
+})
+
+setTimeout(()=> {
+    element.classList.remove('active')
+},exitDelay)
+
+}
+
+function setupanimationcycle({initialScreenIndex,exitDelay}){
+    const cycletime = timePerScreen + exitDelay
+    let nextIndex = 0
+    
 }
 
 setupanimationcycle({
